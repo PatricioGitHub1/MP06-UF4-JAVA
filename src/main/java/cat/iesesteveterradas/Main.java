d1cee214d87b09f52a47c0c19d7701110265d4f5
PatricioGitHub1 <rojas_patricio_github@gmx.es>
2024-04-27 11:13:37 +0200

    commit not compilable

--- src/main/java/cat/iesesteveterradas/Main.java
+++ src/main/java/cat/iesesteveterradas/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
          // Initialize connection details
+         String a = 1;
         String host = "127.0.0.1";
         int port = 1984;
         String username = "admin"; // Default username
